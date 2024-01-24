58caa13e27b43bf524f7f70f342b136847fa719e
kalki7 <anamsaatvik000@gmail.com>
2024-01-24 23:44:01 +0530

    flow added

---
+++ sektch-inevi.js
@@ -0,0 +1,206 @@
+var s1 = function( sketch ){
+
+    let CIRCLE_D = 10;
+    let FSIZE = 80;
+    let BUF = 5;
+
+    let retryCount = 0;
+    let circleHistory = [];
+    let history = [];
+    let mapCheck = {};
+    let map = [];
+    let options = []
+    let circles = [];
+    let rfs;
+    let rbuf;
+    let circle;
+    let numX, numY;
+    let cX, cY;
+    let op;
+    let i, j;
+    let item;
+    let point;
+    let id;
+    let breifHistory;
+    let start = {'x': 0, 'y': 0};
+
+    sketch.setup = function() {
+        let cnv1 = sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
+        cnv1.parent('inevitable')
+        cnv1.style('display', 'block');
+        cnv1.style('inset', 0);
+        sketch.noStroke();
+
+        rfs = (FSIZE / sketch.windowWidth) * sketch.width;
+        rbuf = (BUF / sketch.windowWidth) * sketch.width;
+
+        font = sketch.loadFont('assets/helvetica-compressed-5871d14b6903a.otf');
+
+        numX = sketch.width % CIRCLE_D;
+        numY = sketch.height % CIRCLE_D;
+
+        cX = sketch.width / 2;
+        cY = sketch.height / 2;
+
+        for (i = cX; i < sketch.width - numX; i += CIRCLE_D * 2){
+            for (j = cY; j < sketch.height - numY; j += CIRCLE_D * 2){
+                circle = {
+                    x : i,
+                    y : j,
+                    d : CIRCLE_D
+                }
+                circles.push(circle);
+            }
+        }
+        for (i = sketch.width / 2 - CIRCLE_D * 2; i > 0 + numX; i -= CIRCLE_D * 2){
+            for (j = sketch.height / 2; j < sketch.height - numY; j += CIRCLE_D * 2){
+                circle = {
+                    x : i,
+                    y : j,
+                    d : CIRCLE_D
+                }
+                circles.push(circle);
+            }
+        }
+        for (i = sketch.width / 2; i < sketch.width - numX; i += CIRCLE_D * 2){
+            for (j = sketch.height / 2 - CIRCLE_D * 2; j > 0 + numY; j -= CIRCLE_D * 2){
+                circle = {
+                    x : i,
+                    y : j,
+                    d : CIRCLE_D
+                }
+                circles.push(circle);
+            }
+        }
+        for (i = sketch.width / 2 - CIRCLE_D * 2; i > 0 + numX; i -= CIRCLE_D * 2){
+            for (j = sketch.height / 2 - CIRCLE_D * 2; j > 0 + numY; j -= CIRCLE_D * 2){
+                circle = {
+                    x : i,
+                    y : j,
+                    d : CIRCLE_D
+                }
+                circles.push(circle);
+            }
+        }
+        
+        circles.sort(function(a, b) {
+            return parseFloat(a.y) - parseFloat(b.y);
+        });
+        circles.sort(function(a, b) {
+            return parseFloat(a.x) - parseFloat(b.x);
+        });
+
+        j = -1;
+        for(i = 0; i < circles.length; i++){
+            if (!mapCheck[circles[i].x]) {
+                mapCheck[circles[i].x] = [];
+                map.push([]);
+                j += 1;
+            }
+            map[j].push(circles[i]);
+        }
+        delete mapCheck;
+
+        start.x = Number(sketch.random(0,map.length)).toFixed(0);
+        start.y = Number(sketch.random(0,map[0].length)).toFixed(0);
+        point = start;
+        id = String(point.x) + "," + String(point.y);
+        breifHistory = [id];
+    }
+
+    sketch.draw = function() {
+        sketch.background(30);
+        sketch.fill(200);
+        sketch.frameRate(10);
+        // since the starting point isn't considered
+        sketch.circle(map[start.x][start.y].x, map[start.x][start.y].y, CIRCLE_D);
+
+        // for(i = 0; i < circles.length; i++){
+        //     circle = circles[i];
+        //     sketch.circle(circle.x, circle.y, circle.d);
+        // }
+        // sketch.rect(circles[0].x-1,circles[0].y-1,20,2);
+
+        if(point.x - 1 >= 0){
+            options.push({'x': point.x - 1, 'y': point.y, 'op': 4});
+        }
+        if(point.y + 1 < map[0].length){
+            options.push({'x': point.x, 'y': point.y + 1, 'op': 3});
+        }
+        if(point.x + 1 < map.length){
+            options.push({'x': point.x + 1, 'y': point.y, 'op': 2});
+        }
+        if(point.y - 1 >= 0){
+            options.push({'x': point.x, 'y': point.y - 1, 'op': 1});
+        }
+
+        op = sketch.random(options);
+
+        if(op.op == 2){
+            item = [map[point.x][point.y].x, map[point.x][point.y].y, CIRCLE_D * 2, 2];
+        }
+        else if(op.op == 3){
+            item = [map[point.x][point.y].x, map[point.x][point.y].y, 2, CIRCLE_D * 2];
+        }
+        else if(op.op == 4){
+            item = [map[op.x][op.y].x, map[op.x][op.y].y, CIRCLE_D * 2, 2];
+        }
+        else if(op.op == 1){
+            item = [map[op.x][op.y].x, map[op.x][op.y].y, 2, CIRCLE_D *2];
+        }
+        id = String(op.x) + "," + String(op.y);
+        if(breifHistory.indexOf(id) == -1){
+            // console.log(id," Not done");
+            breifHistory.push(id);
+            circleHistory.push([map[op.x][op.y].x, map[op.x][op.y].y])
+            history.push(item);
+            point = op;
+            options = [];
+            retryCount = 0;
+        }
+        else{
+            // console.log(id, "already done");
+            options = [];
+            retryCount += 1;
+            if(retryCount > 80){
+                sketch.mouseClicked();
+            }
+        }
+
+        for(i = 0; i < history.length; i++){
+            sketch.circle(circleHistory[i][0], circleHistory[i][1], CIRCLE_D);
+            sketch.rect(history[i][0] - 1, history[i][1] - 1, history[i][2], history[i][3]);
+        }
+        
+        sketch.fill(200);
+        sketch.textAlign(sketch.CENTER, sketch.BOTTOM);
+        sketch.textFont(font, rfs);
+        sketch.text('change is inevitable', (sketch.width / 2), (sketch.height / 2) - (rfs - rbuf));
+        sketch.text('go with the flow', (sketch.width / 2), (sketch.height / 2));
+        sketch.text('we will all be dead soon', (sketch.width / 2), (sketch.height / 2) + (rfs - rbuf));
+        sketch.textFont(font, rfs / 4);
+        sketch.text("just like this page, you'll never see the same thing again", (sketch.width / 2), sketch.height - (sketch.height / 4)); 
+
+        
+        
+
+    }
+    
+    sketch.mouseClicked = function(){
+        start.x = Number(sketch.random(0,map.length)).toFixed(0);
+        start.y = Number(sketch.random(0,map[0].length)).toFixed(0);
+        point = start;
+        id = String(point.x) + "," + String(point.y);
+        breifHistory = [id];
+        circleHistory = [];
+        history = [];
+        retryCount = 0;
+        sketch.redraw();
+
+    }
+
+    sketch.windowResized = function() {
+    }
+}
+
+new p5(s1, window.document.getElementById('inevitable'));
--- sektch-infi.js
+++ sektch-infi.js
@@ -1,113 +1,127 @@
-let NUM_CIRCLES = 40;
-let FLOAT_SPEED = 1;
-let TRANSITION_SPEED = 0.05;
-let CIRCLESIZE_D = 100;
-let FSIZE = 160;
-let BUF = 40
+var s1 = function( sketch ){
+    let NUM_CIRCLES = 40;
+    let FLOAT_SPEED = 1;
+    let TRANSITION_SPEED = 0.05;
+    let CIRCLESIZE_D = 100;
+    let FSIZE = 160;
+    let BUF = 40;
 
-let circles = []; 
-let float = true;
-let circleSize;
-let tDiv;
-let a;
-let mouseNorm = 0;
+    let circles = []; 
+    let float = false;
+    let rfs;
+    let rbuf;
+    let circleSize;
+    let tDiv;
+    let a;
+    let mouseNorm = 0;
 
+    sketch.setup = function() {
+        let cnv1 = sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
+        cnv1.parent('head')
+        cnv1.style('display', 'block');
+        cnv1.style('position', 'absolute');
+        cnv1.style('inset', 0);
+        sketch.noStroke();
 
-function gxy(x, y) {
-    return (x - y);
-}
+        font = sketch.loadFont('assets/helvetica-compressed-5871d14b6903a.otf');
 
-function setup(){
-    let cnv = createCanvas(windowWidth, windowHeight);
-    cnv.parent("head")
-    cnv.style('display', 'block');
-    cnv.style('position', 'absolute');
-    cnv.style('inset', 0);
-    noStroke();
-
-    font = loadFont('assets/helvetica-compressed-5871d14b6903a.otf');
-
-    circleSize = width / CIRCLESIZE_D;
-    tDiv = 2 * 3.14 / NUM_CIRCLES;
-    a = width / 10;
-
-    for(let i = 0; i < NUM_CIRCLES; i++){
-        let tI = tDiv * i;
-        let circle = {
-            x : random(circleSize / 2, width - circleSize / 2),
-            y : random(circleSize / 2, height - circleSize / 2),
-            speedX: random(-FLOAT_SPEED, FLOAT_SPEED),
-            speedY: random(-FLOAT_SPEED, FLOAT_SPEED),
-            targetX:  ((a * sqrt(2) * cos(tI)) / (sq(sin(tI)) + 1)) + (width / 2),
-            targetY: (a * sqrt(2) * cos(tI) * sin(tI)) / (sq(sin(tI)) + 1) + height - (height / 6) - FSIZE / 6,
-            tI : tI
-        };
-        circles.push(circle);
-    }
-}
-
-function draw(){
-    if (float){
-        background(30);
-        fill(200);
-        mouseNorm = gxy(mouseX, mouseY);
-
-        textAlign(RIGHT, BOTTOM);
-        textFont(font, FSIZE/3);
-        text("order", (width / 2) - a / 2.5, height - (height / 6));  
-    }
-    else{
-        background(200);
-        fill(30);
-
-        textAlign(LEFT, BOTTOM);
-        textFont(font, FSIZE/3);
-        text("chaos", (width / 2) + a / 2.5, height - (height / 6)); 
-    }
-    textAlign(CENTER, BOTTOM);
-    textFont(font, FSIZE);
-    text('what we', (width / 2) - mouseNorm * 0.3, (height / 2) - (FSIZE - BUF));
-    text('do in the', (width / 2) - mouseNorm * 0.1, (height / 2));
-    text('shadows', (width / 2), (height / 2) + (FSIZE - BUF));
-
-    // textFont(font, FSIZE / 3);
-    // text("|", (width / 2), height - (height / 6));
-
-    for(let i = 0; i < circles.length; i++){
-        let circle = circles[i];
-
-        if(float){
-            
-            circle.x = lerp(circle.x, circle.targetX, TRANSITION_SPEED);
-            circle.y = lerp(circle.y, circle.targetY, TRANSITION_SPEED);
-
+        rfs = (FSIZE / sketch.windowWidth) * sketch.width;
+        rbuf = (BUF / sketch.windowWidth) * sketch.width;
+        circleSize = sketch.width / CIRCLESIZE_D;
+        tDiv = 2 * 3.14 / NUM_CIRCLES;
+        if (sketch.windowWidth > sketch.windowHeight){
+            a = sketch.width / 10;
         }
         else{
-
-            circle.tI += 1/100;
-            circle.x = ((a * sqrt(2) * cos(circle.tI)) / (sq(sin(circle.tI)) + 1)) + (width / 2);
-            circle.y = (a * sqrt(2) * cos(circle.tI) * sin(circle.tI)) / (sq(sin(circle.tI)) + 1) + height - (height / 6) - FSIZE / 6;
-
-            if(circle.tI >= 2 * 3.14){
-                circle.tI = 0;
-            }
-
+            a = sketch.height / 10;
         }
 
-        ellipse(circle.x, circle.y, circleSize, circleSize);
-        
+        for(let i = 0; i < NUM_CIRCLES; i++){
+            let tI = tDiv * i;
+            let circle = {
+                x : sketch.random(circleSize / 2, sketch.width - circleSize / 2),
+                y : sketch.random(circleSize / 2, sketch.height - circleSize / 2),
+                speedX: sketch.random(-FLOAT_SPEED, FLOAT_SPEED),
+                speedY: sketch.random(-FLOAT_SPEED, FLOAT_SPEED),
+                targetX:  ((a * sketch.sqrt(2) * sketch.cos(tI)) / (sketch.sq(sketch.sin(tI)) + 1)) + (sketch.width / 2),
+                targetY: (a * sketch.sqrt(2) * sketch.cos(tI) * sketch.sin(tI)) / (sketch.sq(sketch.sin(tI)) + 1) + sketch.height - (sketch.height / 6) - rfs / 6,
+                tI : tI
+            };
+            circles.push(circle);
+        }
+    }
+
+    sketch.draw = function() {
+        if (float){
+            sketch.background(30);
+            sketch.fill(200);
+            mouseNorm = 0;
+
+            
+        }
+        else{
+            sketch.background(200);
+            sketch.fill(30);
+        }
+        sketch.textAlign(sketch.CENTER, sketch.BOTTOM);
+        sketch.textFont(font, rfs);
+        sketch.text('what we', (sketch.width / 2) - mouseNorm * 0.3, (sketch.height / 2) - (rfs - rbuf));
+        sketch.text('do in the', (sketch.width / 2) - mouseNorm * 0.1, (sketch.height / 2));
+        sketch.text('shadows', (sketch.width / 2), (sketch.height / 2) + (rfs - rbuf));
+
+        sketch.textAlign(sketch.RIGHT, sketch.BOTTOM);
+        sketch.textFont(font, rfs/3);
+        sketch.text("order", (sketch.width / 2) - a / 2.5, sketch.height - (sketch.height / 6));
+
+        sketch.textAlign(sketch.LEFT, sketch.BOTTOM);
+        sketch.textFont(font, rfs/3);
+        sketch.text("chaos", (sketch.width / 2) + a / 2.5, sketch.height - (sketch.height / 6)); 
+
+        // textFont(font, rfs / 3);
+        // text("|", (width / 2), height - (height / 6));
+
+        for(let i = 0; i < circles.length; i++){
+            let circle = circles[i];
+
+            if(float){
+                
+                circle.x = sketch.lerp(circle.x, circle.targetX, TRANSITION_SPEED);
+                circle.y = sketch.lerp(circle.y, circle.targetY, TRANSITION_SPEED);
+
+            }
+            else{
+
+                circle.tI += 1/100;
+                circle.x = ((a * sketch.sqrt(2) * sketch.cos(circle.tI)) / (sketch.sq(sketch.sin(circle.tI)) + 1)) + (sketch.width / 2);
+                circle.y = (a * sketch.sqrt(2) * sketch.cos(circle.tI) * sketch.sin(circle.tI)) / (sketch.sq(sketch.sin(circle.tI)) + 1) + sketch.height - (sketch.height / 6) - rfs / 6;
+
+                if(circle.tI >= 2 * 3.14){
+                    circle.tI = 0;
+                }
+
+            }
+
+            sketch.ellipse(circle.x, circle.y, circleSize, circleSize);
+            
+        }
+    }
+
+    sketch.mouseWheel = function() {
+        float = true;
+    }
+
+    sketch.windowResized = function() {
+        sketch.resizeCanvas(sketch.windowWidth, sketch.windowHeight);
+        rfs = (FSIZE / sketch.windowWidth) * sketch.width;
+        rbuf = (BUF / sketch.windowWidth) * sketch.width;
+        circleSize = sketch.width / CIRCLESIZE_D;
+        if (sketch.windowWidth > sketch.windowHeight){
+            a = sketch.width / 10;
+        }
+        else{
+            a = sketch.height / 10;
+        }
     }
 }
 
-// function mouseWheel(){
-//     float = false;
-// }
-
-function mouseClicked(){
-    float = !float;
-}
-
-function windowResized(){
-    resizeCanvas(windowWidth, windowHeight);
-    circleSize = width / CIRCLESIZE_D;
-}
+new p5(s1, window.document.getElementById('head'));
