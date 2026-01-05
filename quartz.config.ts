125d4b2c7a549149aa4741f44be3f3a25a8250b3
kalki7 <anamsaatvik000@gmail.com>
2026-01-05 12:30:48 +0530

    deply

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "wwdits",
+    baseUrl: "www.whatwedointheshadows.in",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
